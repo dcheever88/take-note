63aad042498d91451b5f1e086b91165fc8d132bf
dcheever <cheeverdanny@gmail.com>
2021-05-15 16:24:04 -0500

    add server.js

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require("./routes/appRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`API server now on port ${PORT}!`);
+});
