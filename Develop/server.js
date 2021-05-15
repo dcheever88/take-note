2b2375d6a7e3afcbe63a21f8cc7f527f19e581d3
dcheever <cheeverdanny@gmail.com>
2021-05-15 18:23:03 -0500

    trying heroku 3

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
