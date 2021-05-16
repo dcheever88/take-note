7f3c5c57dc26903d82a0af510c41087698215a77
dcheever <cheeverdanny@gmail.com>
2021-05-15 19:00:38 -0500

    route 4

--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require("./public/routes/appRoutes")(app);
+require("./routes/appRoutes")(app);
 
 app.listen(PORT, function() {
     console.log(`API server now on port ${PORT}!`);
